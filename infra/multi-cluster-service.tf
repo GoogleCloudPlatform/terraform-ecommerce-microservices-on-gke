091fb20d2b41c64f908b6e22cc7b99f5cccd17c7
Nim Jayawardena <nimjay@google.com>
2023-03-17 14:21:45 -0400

    Add Terraform for Multi Cluster Ingress

---
+++ infra/multi-cluster-service.tf
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "google_gke_hub_feature" "multi_cluster_ingress_feature" {
+  name = "multiclusteringress"
+  location = "global"
+  spec {
+    multiclusteringress {
+      config_membership = google_gke_hub_membership.my_fleet_membership_config.id
+    }
+  }
+  provider = google-beta
+}
