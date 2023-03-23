693c0262d9706fcac40611e993f55b5f346e1462
Nim Jayawardena <nimjay@google.com>
2023-03-23 09:37:05 -0400

    Use snake_case for infra/ files
    This is consistent with the file/folder naming convention used by the
    Blueprints team.

--- infra/deploy_kubernetes_manifests.tf
+++ infra/deploy_kubernetes_manifests.tf
@@ -2,7 +2,7 @@
 resource "null_resource" "deploy_kubernetes_manifests" {
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
-    command     = "chmod +x deploy-kubernetes-manifests.sh;./deploy-kubernetes-manifests.sh ${var.resource_name_suffix}"
+    command     = "chmod +x deploy_kubernetes_manifests.sh;./deploy_kubernetes_manifests.sh ${var.resource_name_suffix}"
   }
   depends_on = [
     resource.google_project_iam_member.gke_mcs_importer_iam_binding
