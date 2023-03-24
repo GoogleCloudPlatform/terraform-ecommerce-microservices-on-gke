c9286e683ff2aa039195477f5b2eb3ce28ca64b8
Nim Jayawardena <nimjay@google.com>
2023-03-24 15:15:32 -0400

    Fix mixed up file names
    This commit merely switches the contents of the two .sh files.

--- infra/deploy_multi_cluster_k8s_resources.sh
+++ infra/deploy_multi_cluster_k8s_resources.sh
@@ -2,38 +2,15 @@
 PROJECT_ID=$1
 RESOURCE_NAME_SUFFIX=$2
 
-CLUSTER_CONTEXT_CANADA=gke_${PROJECT_ID}_northamerica-northeast1_my-cluster-canada${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_CONFIG=gke_${PROJECT_ID}_us-west1_my-cluster-config${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_USA=gke_${PROJECT_ID}_us-west1_my-cluster-usa${RESOURCE_NAME_SUFFIX}
 K8S_MANIFESTS_DIR=../kubernetes_manifests
 
-# Connect to the 3 clusters that we just created.
-gcloud container clusters get-credentials my-cluster-canada${RESOURCE_NAME_SUFFIX} \
-  --project ${PROJECT_ID} \
-  --region northamerica-northeast1
-gcloud container clusters get-credentials my-cluster-usa${RESOURCE_NAME_SUFFIX} \
-  --project ${PROJECT_ID} \
-  --region us-west1
-gcloud container clusters get-credentials my-cluster-config${RESOURCE_NAME_SUFFIX} \
-  --project ${PROJECT_ID} \
-  --region us-west1
-
-app_namespaces=(adservice cartservice checkoutservice currencyservice emailservice frontend paymentservice productcatalogservice recommendationservice shippingservice)
-
-# Create namespaces.
-# Some Namespaces (especially in my-cluster-config) will be unused.
-kubectl --context=${CLUSTER_CONTEXT_USA} \
-  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
-kubectl --context=${CLUSTER_CONTEXT_CANADA} \
-  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
+# Deploy Multi Cluster Ingress configuration.
 kubectl --context=${CLUSTER_CONTEXT_CONFIG} \
-  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
+  apply -f ${K8S_MANIFESTS_DIR}/multi_cluster_ingress.yaml
 
-# Deploy most of the microservices.
-for namespace in "${app_namespaces[@]}";
-do
-  kubectl --context=${CLUSTER_CONTEXT_USA} \
-    apply -f ${K8S_MANIFESTS_DIR}/${namespace}/
-  kubectl --context=${CLUSTER_CONTEXT_CANADA} \
-    apply -f ${K8S_MANIFESTS_DIR}/${namespace}/
-done
+# Deploy the redis-cart Service into the US cluster.
+# This redis-cart Service gets exported to the other clusters.
+kubectl --context=${CLUSTER_CONTEXT_USA} \
+  apply -f ${K8S_MANIFESTS_DIR}/redis_cart/
--- infra/deploy_single_cluster_k8s_resources.sh
+++ infra/deploy_single_cluster_k8s_resources.sh
@@ -2,15 +2,38 @@
 PROJECT_ID=$1
 RESOURCE_NAME_SUFFIX=$2
 
+CLUSTER_CONTEXT_CANADA=gke_${PROJECT_ID}_northamerica-northeast1_my-cluster-canada${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_CONFIG=gke_${PROJECT_ID}_us-west1_my-cluster-config${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_USA=gke_${PROJECT_ID}_us-west1_my-cluster-usa${RESOURCE_NAME_SUFFIX}
 K8S_MANIFESTS_DIR=../kubernetes_manifests
 
-# Deploy Multi Cluster Ingress configuration.
-kubectl --context=${CLUSTER_CONTEXT_CONFIG} \
-  apply -f ${K8S_MANIFESTS_DIR}/multi_cluster_ingress.yaml
+# Connect to the 3 clusters that we just created.
+gcloud container clusters get-credentials my-cluster-canada${RESOURCE_NAME_SUFFIX} \
+  --project ${PROJECT_ID} \
+  --region northamerica-northeast1
+gcloud container clusters get-credentials my-cluster-usa${RESOURCE_NAME_SUFFIX} \
+  --project ${PROJECT_ID} \
+  --region us-west1
+gcloud container clusters get-credentials my-cluster-config${RESOURCE_NAME_SUFFIX} \
+  --project ${PROJECT_ID} \
+  --region us-west1
 
-# Deploy the redis-cart Service into the US cluster.
-# This redis-cart Service gets exported to the other clusters.
+app_namespaces=(adservice cartservice checkoutservice currencyservice emailservice frontend paymentservice productcatalogservice recommendationservice shippingservice)
+
+# Create namespaces.
+# Some Namespaces (especially in my-cluster-config) will be unused.
 kubectl --context=${CLUSTER_CONTEXT_USA} \
-  apply -f ${K8S_MANIFESTS_DIR}/redis_cart/
+  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
+kubectl --context=${CLUSTER_CONTEXT_CANADA} \
+  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
+kubectl --context=${CLUSTER_CONTEXT_CONFIG} \
+  apply -f ${K8S_MANIFESTS_DIR}/namespaces.yaml
+
+# Deploy most of the microservices.
+for namespace in "${app_namespaces[@]}";
+do
+  kubectl --context=${CLUSTER_CONTEXT_USA} \
+    apply -f ${K8S_MANIFESTS_DIR}/${namespace}/
+  kubectl --context=${CLUSTER_CONTEXT_CANADA} \
+    apply -f ${K8S_MANIFESTS_DIR}/${namespace}/
+done
