6824cb26c7c67129a06c3499d016a8a11e1ca8db
Nim Jayawardena <nimjay@google.com>
2023-04-04 16:23:35 -0400

    Use path.module for local-execs

--- infra/deploy_multi_cluster_k8s_resources.sh
+++ infra/deploy_multi_cluster_k8s_resources.sh
@@ -17,11 +17,11 @@
 # Script parameters
 PROJECT_ID=$1
 RESOURCE_NAME_SUFFIX=$2
+K8S_MANIFESTS_DIR=$3
 
 CLUSTER_CONTEXT_CANADA=gke_${PROJECT_ID}_northamerica-northeast1_my-cluster-canada${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_CONFIG=gke_${PROJECT_ID}_us-west1_my-cluster-config${RESOURCE_NAME_SUFFIX}
 CLUSTER_CONTEXT_USA=gke_${PROJECT_ID}_us-west1_my-cluster-usa${RESOURCE_NAME_SUFFIX}
-K8S_MANIFESTS_DIR=../kubernetes_manifests
 
 wait_for_crd() {
   CRD=$1
