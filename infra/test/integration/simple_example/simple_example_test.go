5f53be648f4e50a55d1741626b42b5cd966c0ce2
Nim Jayawardena <nimjay@google.com>
2023-04-12 08:36:16 -0400

    Test for deployment IP address (#15)

--- infra/test/integration/simple_example/simple_example_test.go
+++ infra/test/integration/simple_example/simple_example_test.go
@@ -16,7 +16,10 @@ package simple_example
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
@@ -32,29 +35,61 @@ func TestSimpleExample(t *testing.T) {
 
 	example.DefineVerify(func(assert *assert.Assertions) {
 		projectId := example.GetTFSetupStringOutput("project_id")
-		// TODO: Test the deploymentIpAddress.
-		// deploymentIpAddress := example.GetStringOutput("deployment_ip_address")
-
-		// Test that GCP APIs have been enabled.
-		// TODO: Change this serviceTests in an array of strings.
-		serviceTests := map[string]struct {
-			service string
-		}{
-			"Service container":                    {service: "container"},
-			"Service dns":                          {service: "dns"},
-			"Service gkehub":                       {service: "gkehub"},
-			"Service multiclusteringress":          {service: "multiclusteringress"},
-			"Service multiclusterservicediscovery": {service: "multiclusterservicediscovery"},
-			"Service trafficdirector":              {service: "trafficdirector"},
-		}
-		services := gcloud.Run(t, "services list", gcloud.WithCommonArgs([]string{"--project", projectId, "--format", "json"})).Array()
-		for _, tc := range serviceTests {
-			t.Run(tc.service, func(t *testing.T) {
-				match := utils.GetFirstMatchResult(t, services, "config.name", fmt.Sprintf("%s.googleapis.com", tc.service))
-				assert.Equal("ENABLED", match.Get("state").String(), "%s service should be enabled", tc.service)
-			})
-		}
+		deploymentIpAddr := example.GetStringOutput("deployment_ip_address")
+		deploymentUrl := fmt.Sprintf("http://%s", deploymentIpAddr)
+		testDeploymentUrl(t, assert, deploymentUrl)
+		testGoogleCloudApis(t, assert, projectId)
 	})
 
 	example.Test()
+}
+
+func testDeploymentUrl(t *testing.T, assert *assert.Assertions, url string) error {
+	for attemptNum := 1; attemptNum <= 60; attemptNum++ {
+
+		// Failed request.
+		response, err := http.Get(url)
+		if err != nil {
+			t.Logf("Deployment URL HTTP request error: %s\n", err)
+
+		} else if 200 <= response.StatusCode && response.StatusCode <= 299 { // Got some 200 responses.
+			responseBody, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				return err
+			}
+			responseBodyString := string(responseBody)
+			assert.Containsf(responseBodyString, "us-west1", "couldn't find text 'us-west1' in deployment's response")
+			assert.Containsf(responseBodyString, "Cymbal Shops", "Couldn't find text 'Cymbal Shops' in deployment's response")
+			return nil
+
+		} else { // Got a non-200 response.
+			t.Logf("Deployment URL responded with status code: %d.\n", response.StatusCode)
+		}
+
+		// Wait before retrying.
+		time.Sleep(4 * time.Second)
+	}
+
+	t.Logf("Waited too long for deployment URL.\n")
+	return nil
+}
+
+func testGoogleCloudApis(t *testing.T, assert *assert.Assertions, projectId string) {
+	serviceTests := map[string]struct {
+		service string
+	}{
+		"Service container":                    {service: "container"},
+		"Service dns":                          {service: "dns"},
+		"Service gkehub":                       {service: "gkehub"},
+		"Service multiclusteringress":          {service: "multiclusteringress"},
+		"Service multiclusterservicediscovery": {service: "multiclusterservicediscovery"},
+		"Service trafficdirector":              {service: "trafficdirector"},
+	}
+	services := gcloud.Run(t, "services list", gcloud.WithCommonArgs([]string{"--project", projectId, "--format", "json"})).Array()
+	for _, tc := range serviceTests {
+		t.Run(tc.service, func(t *testing.T) {
+			match := utils.GetFirstMatchResult(t, services, "config.name", fmt.Sprintf("%s.googleapis.com", tc.service))
+			assert.Equal("ENABLED", match.Get("state").String(), "%s service should be enabled", tc.service)
+		})
+	}
 }
