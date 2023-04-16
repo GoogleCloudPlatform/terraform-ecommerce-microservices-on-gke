9aceadb3f0f1cceea3d6333edf251b14fbb1038a
Nim Jayawardena <nimjay@google.com>
2023-04-16 14:08:38 -0400

    Use false load_config_file in kubernetes provider
    This commit hopes to fix an error with terraform destroy:
    Error: Get http://localhost/api/v1/namespaces/default/serviceaccounts/k8s-manifests-deployer-service-account": dial tcp 127.0.0.1:80: connect: connection refused

--- infra/modules/k8s_manifests_deployer_job/k8s_manifests_deployer_job.tf
+++ infra/modules/k8s_manifests_deployer_job/k8s_manifests_deployer_job.tf
@@ -40,6 +40,7 @@ provider "kubernetes" {
   host                   = var.cluster_host
   token                  = var.cluster_token
   cluster_ca_certificate = var.cluster_ca_certificate
+  load_config_file       = false
   alias                  = "kubernetes_provider"
 }
 
