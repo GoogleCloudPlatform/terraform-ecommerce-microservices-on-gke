cde48ffcd18189ddf98e8a71e44e3eabf1538673
Nim Jayawardena <nimjay@google.com>
2023-04-16 15:10:07 -0400

    Remove use of load_config_file in k8s provider
    It's no longer supported.

--- infra/modules/k8s_manifests_deployer_job/k8s_manifests_deployer_job.tf
+++ infra/modules/k8s_manifests_deployer_job/k8s_manifests_deployer_job.tf
@@ -40,7 +40,6 @@ provider "kubernetes" {
   host                   = var.cluster_host
   token                  = var.cluster_token
   cluster_ca_certificate = var.cluster_ca_certificate
-  load_config_file       = false
   alias                  = "kubernetes_provider"
 }
 
