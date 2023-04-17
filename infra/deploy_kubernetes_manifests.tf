68b807df6ca9a988b1240d6b07220822fe67f1dd
Nim Jayawardena <nimjay@google.com>
2023-04-17 11:27:04 -0400

    Remove unused data "google_client_config"

--- infra/deploy_kubernetes_manifests.tf
+++ infra/deploy_kubernetes_manifests.tf
@@ -23,9 +23,6 @@ locals {
   google_service_account_id = "k8s-manifests-deployer${var.resource_name_suffix}"
 }
 
-// Enable access to the configuration of the Google Cloud provider.
-data "google_client_config" "default" {}
-
 // Connect a Kubernetes provider to the config cluster.
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.my_cluster_config.endpoint}"
