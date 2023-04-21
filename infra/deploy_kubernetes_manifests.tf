871f8393c5bb51c80c76f8c675d2870151fda822
Nim Jayawardena <nimjay@google.com>
2023-04-20 23:16:11 -0400

    feat: Replace `local-exec` with K8s job that deploys all K8s resources  (#22)

--- infra/deploy_kubernetes_manifests.tf
+++ infra/deploy_kubernetes_manifests.tf
@@ -14,25 +14,121 @@
  * limitations under the License.
  */
 
-resource "null_resource" "deploy_single_cluster_k8s_resources" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "chmod +x ${path.module}/deploy_single_cluster_k8s_resources.sh;${path.module}/deploy_single_cluster_k8s_resources.sh ${var.project_id} ${var.resource_name_suffix} ${path.module}/../kubernetes_manifests"
+// This file deploys a Kubernetes Job to the config cluster.
+// That Job will deploy Kubernetes resources to all clusters.
+
+locals {
+  k8s_deployer_namespace    = "default"
+  k8s_service_account_name  = "k8s-manifests-deployer-service-account"
+  google_service_account_id = "k8s-manifests-deployer${var.resource_name_suffix}"
+}
+
+// Recreate the existing kubernetes_provider.tf file so that the
+// credentials to the cluster are hard-coded.
+data "template_file" "kubernetes_provider" {
+  template = file("${path.module}/kubernetes_provider.tf.template")
+  vars = {
+    cluster_host           = "https://${google_container_cluster.my_cluster_config.endpoint}"
+    cluster_token          = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.my_cluster_config.master_auth[0].cluster_ca_certificate)
+  }
+}
+resource "local_sensitive_file" "kubernetes_provider" {
+  content  = data.template_file.kubernetes_provider.rendered
+  filename = "${path.module}/kubernetes_provider.tf"
+  lifecycle {
+    ignore_changes = [
+      content,
+      filename,
+      directory_permission,
+      file_permission,
+    ]
+  }
+}
+
+// Kubernetes (K8s) Job inside the cluser that deploys K8s resources to all clusters.
+resource "kubernetes_job" "kubernetes_manifests_deployer_job" {
+  metadata {
+    name      = "kubernetes-manifests-deployer-job"
+    namespace = local.k8s_deployer_namespace
+  }
+  spec {
+    completions = 1
+    template {
+      metadata {}
+      spec {
+        service_account_name = local.k8s_service_account_name
+        container {
+          name  = "kubernetes-manifests-deployer"
+          image = "us-docker.pkg.dev/google-samples/containers/gke/kubernetes-manifests-deployer:v0.0.0.0"
+          env {
+            name  = "PROJECT_ID"
+            value = var.project_id
+          }
+          env {
+            name  = "RESOURCE_NAME_SUFFIX"
+            value = var.resource_name_suffix
+          }
+          resources {
+            limits = {
+              cpu    = "250m"
+              memory = "128Mi"
+            }
+            requests = {
+              cpu    = "100m"
+              memory = "64Mi"
+            }
+          }
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+  wait_for_completion = true
+  timeouts {
+    create = "600s"
   }
   depends_on = [
-    resource.google_container_cluster.my_cluster_europe,
-    resource.google_container_cluster.my_cluster_config,
-    resource.google_container_cluster.my_cluster_usa,
+    google_project_iam_member.google_service_account_is_kubernetes_admin,
+    google_service_account_iam_member.allow_kubernetes_sa_to_impersonate_google_cloud_sa,
+    kubernetes_service_account.kubernetes_manifests_deployer_service_account
   ]
 }
 
-resource "null_resource" "deploy_multi_cluster_k8s_resources" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-exc"]
-    command     = "chmod +x ${path.module}/deploy_multi_cluster_k8s_resources.sh;${path.module}/deploy_multi_cluster_k8s_resources.sh ${var.project_id} ${var.resource_name_suffix} ${path.module}/../kubernetes_manifests"
+// Kubernetes Service Account (different from the Google Cloud Service Account).
+resource "kubernetes_service_account" "kubernetes_manifests_deployer_service_account" {
+  metadata {
+    name      = local.k8s_service_account_name
+    namespace = local.k8s_deployer_namespace
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.kubernetes_manifests_deployer_service_account.email
+    }
   }
+}
+
+// The Google Cloud Service Account.
+resource "google_service_account" "kubernetes_manifests_deployer_service_account" {
+  project      = var.project_id
+  account_id   = local.google_service_account_id
+  display_name = "Kubernetes Manifests Deployer"
   depends_on = [
-    resource.google_project_iam_member.gke_mcs_importer_iam_binding,
-    resource.null_resource.deploy_single_cluster_k8s_resources,
+    module.enable_google_apis
+  ]
+}
+
+// The Google Cloud Service Account needs to administer Kubernetes resource in all clusters.
+resource "google_project_iam_member" "google_service_account_is_kubernetes_admin" {
+  project = var.project_id
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.kubernetes_manifests_deployer_service_account.email}"
+}
+
+// Allow the Kubernetes Service Account to impersonate the Google Cloud Service Account.
+resource "google_service_account_iam_member" "allow_kubernetes_sa_to_impersonate_google_cloud_sa" {
+  service_account_id = google_service_account.kubernetes_manifests_deployer_service_account.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[${local.k8s_deployer_namespace}/${local.k8s_service_account_name}]"
+  depends_on = [
+    kubernetes_service_account.kubernetes_manifests_deployer_service_account
   ]
 }
