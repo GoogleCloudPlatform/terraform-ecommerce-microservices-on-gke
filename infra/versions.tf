ba014e4aaa713f65050cc4dd4b619a2eb86a4b2e
Don McCasland <patatha@gmail.com>
2023-07-26 08:15:34 -0700

    fix: pinning google provider < 4.75.0

--- infra/versions.tf
+++ infra/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.25.0"
+      version = ">= 3.25.0, <= 4.74, != 4.75.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
