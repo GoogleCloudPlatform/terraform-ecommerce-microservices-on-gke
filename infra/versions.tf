dfe98fa375989a7e7c8c0c38292cd3de898191ae
Nim Jayawardena <nimjay@google.com>
2023-11-06 13:08:57 -0500

    Remove "v" prefix in versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> v5.4"
+      version = "~> 5.4"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
